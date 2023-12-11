e6960f49148e1f731bdc2d731a811b4000c512d5
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-11 15:55:29 +0100

    random number generator

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const number = document.querySelector(".number");
+const button = document.querySelector(".button");
+
+const generateNumber = () => {
+  const generatedNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = generatedNumber;
+  styleNumber();
+};
+button.addEventListener("click", generateNumber);
+const styleNumber = () => {
+  if (number.innerHTML > 5) {
+    number.style.color = "red";
+  } else {
+    number.style.color = "green";
+  }
+};
